157a691b5e67dbf6769d84759bf57817aba1b9ab
Denis Tsvetkov <dtsvetkov@servicechannel.com>
2021-01-12 19:13:59 +0200

    Add QuickSort

--- SortingService.API/Logic/QuickSorter.cs
+++ SortingService.API/Logic/QuickSorter.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using SortingService.API.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SortingService.API.Logic
 {
-    public class QuickSorter
+    public class QuickSorter : ISortingService
     {
+        private int[] _data;
+        public void Sort(int[] data)
+        {
+            _data = data;
+            PartitionReorder(0, data.Length - 1);
+        }
 
+        public void PartitionReorder(int startIndex, int endIndex)
+        {
+            int pivotIndex = endIndex;
+
+            if (endIndex == startIndex)
+                return;
+
+            for (int i = startIndex; i <= pivotIndex - 1; )
+            {
+                if (_data[i] > _data[pivotIndex])
+                {
+                    //swap
+                    int largerThanPivot = _data[i];                    
+                    int pivot = _data[pivotIndex];                                       
+
+                    //only more than one item before the pivot
+                    if (i != pivotIndex - 1)
+                    {
+                        int nearestToPivot = _data[pivotIndex - 1];
+                        _data[i] = nearestToPivot;
+                    }
+
+                    _data[pivotIndex - 1] = pivot;
+                    _data[pivotIndex] = largerThanPivot;
+
+                    pivotIndex -= 1;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            Debug.WriteLine("Result reordering = " + string.Join(",", _data.Take(_data.Length)));
+
+            //left
+            if (pivotIndex != 0 && startIndex != pivotIndex)
+            {                
+                PartitionReorder(startIndex, pivotIndex - 1);
+            }
+
+            //right
+            if (pivotIndex != _data.Length - 1)
+            {               
+                PartitionReorder(pivotIndex + 1, endIndex);
+            }
+        }
     }
 }
--- SortingService.Tests/SortingTests.cs
+++ SortingService.Tests/SortingTests.cs
@@ -16,6 +16,21 @@ namespace SortingService.Tests
             sorter.Sort(heap);
 
             Assert.Equal(heap, expectedArray);
-        }        
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 7, 2, 9, 6, 1 }, new int[] { 1, 2, 4, 6, 7, 9 })]
+        [InlineData(new int[] { 5, 2, 8, 10, 1 }, new int[] { 1, 2, 5, 8, 10 })]
+        [InlineData(new int[] { 100, 2, 1201, 3, 444, 343434, 232 }, new int[] { 2, 3, 100, 232, 444, 1201, 343434 })]
+        [InlineData(new int[] { 7, 2, 8 }, new int[] { 2, 7, 8 })]
+        [InlineData(new int[] {7, 2 }, new int[] { 2, 7 })]
+        [InlineData(new int[] { 5 }, new int[] { 5 })]
+        public void QuickSortingTest(int[] heap, int[] expectedArray)
+        {
+            ISortingService sorter = new QuickSorter();
+            sorter.Sort(heap);
+
+            Assert.Equal(heap, expectedArray);
+        }
     }
 }
