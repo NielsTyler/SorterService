fc4cf4f1ededdc58e3c040998e5cb2b9e6cd5ce6
Denis Tsvetkov <dtsvetkov@servicechannel.com>
2021-01-14 23:05:15 +0200

    Add validation for data format

--- SortingService.API/Models/NumbersSet.cs
+++ SortingService.API/Models/NumbersSet.cs
@@ -8,7 +8,10 @@ namespace SortingService.API.Models
 {
     public class NumbersSet
     {
+        private const string onlyFoIntegersConstraint = @"^\d+( \d+)*$";
+
         [Required]
+        [RegularExpression(onlyFoIntegersConstraint, ErrorMessage = "Data string in wrong format or contains non-integer items.(Should be string of integer with spaces)")]
         public string Numbers { get; set; }
     }
 }
